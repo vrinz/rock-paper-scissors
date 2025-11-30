d92c7815b1f2939540491f6910d75464e2322ed4
Vrinz <19779901+vrinz@users.noreply.github.com>
2025-11-30 22:32:56 +0800

    Fixed issue with variables inside function scope

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 	let randomizer = Math.floor(Math.random() * 3);
 	let computerChoice;
@@ -26,30 +29,33 @@ function playRound(humanChoice, computerChoice) {
 	if (humanChoice == "rock" && computerChoice == "scissors") {
 		humanScore += 1;
 		console.log(`You Win!, ${humanChoice} beats ${computerChoice}`);
-	} else if (humanChoice == "paper" && computerChoice == "rock") {
+	} 
+	else if (humanChoice == "paper" && computerChoice == "rock") {
 		humanScore += 1;
 		console.log(`You Win!, ${humanChoice} beats ${computerChoice}`);
-	} else if (humanChoice == "scissors" && computerChoice == "paper") {
+	} 
+	else if (humanChoice == "scissors" && computerChoice == "paper") {
 		humanScore += 1;
 		console.log(`You Win!, ${humanChoice} beats ${computerChoice}`);
-	} else if (humanChoice == computerChoice) {
+	} 
+	else if (humanChoice == computerChoice) {
 		console.log("You are Tied!");
-	} else {
+	} 
+	else {
 		computerScore += 1;
 		console.log(`You Lose!, ${computerChoice} beats ${humanChoice}`);
 	}
 }
 
 function playGame() {
-	let humanScore = 0;
-	let computerScore = 0;
 	for (let i = 0; i < 5; i++) {
 		playRound(getHumanChoice(), getComputerChoice());
 	}
 	if (humanScore > computerScore) {
 		alert("You Win!");
 		console.log("Human Wins!");
-	} else {
+	} 
+	else {
 		alert("You Lose!");
 		console.log("NPC Wins!");
 	}
